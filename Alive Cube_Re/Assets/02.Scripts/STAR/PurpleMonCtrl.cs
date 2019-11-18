5b82de594871e4b85fa7d3cc9de30e4c29de8015
beyoll <veroxx@naver.com>
2019-11-18 09:24:32 +0900

    Stg2

--- Alive Cube_Re/Assets/02.Scripts/STAR/PurpleMonCtrl.cs
+++ Alive Cube_Re/Assets/02.Scripts/STAR/PurpleMonCtrl.cs
@@ -9,31 +9,49 @@ public class PurpleMonCtrl : MonoBehaviour
     //시간차 공격을 위한 시작시간 지연
     public float delayAnimation;
     //공격모드일 때 각도
-    public Vector3 AttackAngle;
+    public Vector3 attackAngle;
+
+    //오디오 
+    private AudioSource audioSource;
+    public AudioClip[] audioClip;
+    public GameObject hitSound;
 
     private Animator animator;
-    private GameObject PurpleLaser;
-
     //애니메이터 파라미터의 문자열을 해시값으로 추출 
-    private readonly int IsStrafe = Animator.StringToHash("Is Strafe");
-    private readonly int TakeDamage = Animator.StringToHash("Take Damage");
-    private readonly int Die = Animator.StringToHash("Die");
+    private readonly int h_Strafe = Animator.StringToHash("Is Strafe");
+    private readonly int h_TakeDamage = Animator.StringToHash("Take Damage");
+    private readonly int h_Die = Animator.StringToHash("Die");
+
+    private GameObject purpleLaser;
+    private GameObject deadParticle;
+    private GameObject rigHub;
 
     void Awake()
     {
         animator = GetComponent<Animator>();
-        PurpleLaser = transform.GetChild(0).gameObject.transform.GetChild(0).gameObject;
+        audioSource = GetComponent<AudioSource>();
+        
+        purpleLaser = transform.GetChild(0).gameObject.transform.GetChild(0).gameObject;
+        deadParticle = transform.GetChild(1).gameObject;
+        rigHub = transform.GetChild(0).gameObject;
+        //redMonBody = transform.GetChild(2).gameObject; //Tentacles 폴더
     }
     private void Start()
     {
-        Invoke("AttackAction", delayAnimation);
+        Invoke("StateAttack", delayAnimation);
     }
 
-    private void AttackAction()
+    private void StateAttack()
     {
         //앞쪽으로 AttackMode만큼 기울어지며 좌우 레이저 빔 
-        transform.DORotate(AttackAngle, 1.0f);
-        animator.SetTrigger(IsStrafe);
-        PurpleLaser.SetActive(true);
+        transform.DORotate(attackAngle, 1.0f);
+        animator.SetTrigger(h_Strafe);
+        purpleLaser.SetActive(true);
+    }
+
+    private void StateDie()
+    {
+        rigHub.SetActive(false);
+        deadParticle.SetActive(true);
     }
 }
